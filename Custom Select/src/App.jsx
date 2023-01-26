702036a43f3a787c71ff6fbb50845ef6af767c1b
youssef el faquir <youssef1998islam@gmail.com>
2023-01-26 10:58:55 +0100

    initial Commit

---
+++ Custom Select/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import CustomSelect from './Components/CustomSelect'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div> Initial </div>
+  )
+}
+
+export default App
